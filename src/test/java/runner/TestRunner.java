5c38484029698dfcd0bc35656901f13d76e16999
Anam2606 <79849517+Anam2606@users.noreply.github.com>
2021-03-02 18:43:06 +0530

    runner dummy

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources",
+        features = "src/test/resources/test1",
         glue = {"amazon.stepDefinitions", "amazon.hooks"},
         dryRun = false,
         monochrome = true,
